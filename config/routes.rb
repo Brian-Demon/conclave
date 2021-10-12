622600f487ed050f9180d571fc59687c07bee5cd
Brian-Demon <BrianWMatejka@gmail.com>
2021-10-12 16:04:44 -0500

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  delete '/logout', to: 'sessions#destroy', as: :logout
-
+  
   # ROOT
   root to: 'home#index'
 
-  #LOGIN
+  # LOGIN / LOGOUT
   get "/auth/google_oauth2/callback" => "sessions#create"
+  delete '/logout', to: 'sessions#destroy', as: :logout
 end
