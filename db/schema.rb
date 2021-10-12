445e4e599e9bc9ceec8b14046589b886a8125524
Brian-Demon <BrianWMatejka@gmail.com>
2021-10-12 17:17:47 -0500

    generate migration to add description to category

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_211650) do
+ActiveRecord::Schema.define(version: 2021_10_12_220309) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "position"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "comments", force: :cascade do |t|
