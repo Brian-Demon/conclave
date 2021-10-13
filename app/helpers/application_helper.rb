39745dc0c03d500ec745ee97f254d0254638475d
Brian-Demon <BrianWMatejka@gmail.com>
2021-10-13 13:36:44 -0500

    Improve prettiness of UI for discussions: - Create `user_avatar` method that shows the user's Google avatar - Refine the color of the borders for the discussion show page - Add information including: # of posts & "posted on" - Add `post_count` method for the `user` model (comments + discussions)
    Co-authored-by: Andrew Nordman <cadwallion@users.noreply.github.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def user_avatar(user, width: 32)
+    if user.image
+      image_tag(user.image, width: width)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_223143) do
+ActiveRecord::Schema.define(version: 2021_10_12_231121) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_10_12_223143) do
     t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   add_foreign_key "comments", "discussions"
