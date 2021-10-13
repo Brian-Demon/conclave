a40c38ae274076f4fbfd3dc9f82da3887b5730c8
Brian-Demon <BrianWMatejka@gmail.com>
2021-10-13 14:17:38 -0500

    discussion validation: - both a category and user must be associated

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -2,4 +2,7 @@ class Discussion < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :comments
+
+  validates :category, presence: true
+  validates :user, presence: true
 end
