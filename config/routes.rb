d13ad9dcb005ef1ecaebe83ddc2e0b0b9454b835
Brian-Demon <BrianWMatejka@gmail.com>
2021-10-13 14:20:11 -0500

    create comments resources route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'comments/edit'
+  get 'comments/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # ROOT
@@ -12,4 +16,7 @@ Rails.application.routes.draw do
   resources :categories do
     resources :discussions
   end
+
+  # COMMENT
+  resources :comments, only: [:create, :destroy, :edit, :update]
 end
