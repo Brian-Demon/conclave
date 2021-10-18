ef8a7d18b255912fdbf03075716e77e7acfe2d2c
Brian-Demon <BrianWMatejka@gmail.com>
2021-10-18 18:30:40 -0500

    Unit test for Category model: - cover when a category is valid/invalid (name/no name)

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,17 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "is valid" do
+    category = Category.new(name: "Test")
+
+    assert category.valid?
+  end
+
+  test "is invalid when no name is present" do
+    category = Category.new(name: nil)
+
+    refute category.valid?
+    assert_not_nil category.errors[:name], 'no validation error for name present'
+  end
 end
