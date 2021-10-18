771709a5eb80e8b684c9e2e411f48d5dfc2c1bf1
Brian-Demon <BrianWMatejka@gmail.com>
2021-10-18 16:48:27 -0500

    `application.js`: - refactor using variables for elements - change `docReady` with additional conditionals - remove "Turbolink"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,53 +4,61 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 
 import "stylesheets/application"
 
 function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
+  // see if DOM is already available
+  if(document.readyState !="loading") {
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", fn);
+  } else {
+    document.attachEvent("onreadystatechange", function() {
+      if(document.readyState =="complete") {
+        fn();
+      }
+    })
+  }
 }
 
 docReady(function() {
-  let commentPanes = document.getElementsByClassName("comment-pane-content");
-  document.getElementById("comment-preview-toggle").addEventListener("click", (e) => {
-    document.getElementById("comment-form").style.display = "none";
-    document.getElementById("comment-form-toggle").classList.remove("active");
-    document.getElementById("comment-preview-toggle").classList.add("active");
+  let previewToggle = document.getElementById("comment-preview-toggle");
+  if (previewToggle !== null) {
+    previewToggle.addEventListener("click", (e) => {
+      document.getElementById("comment-form").style.display = "none";
+      document.getElementById("comment-form-toggle").classList.remove("active");
+      document.getElementById("comment-preview-toggle").classList.add("active");
 
-    let content = document.getElementById("comment_body").value;
-    let csrf_token = document.getElementsByName("authenticity_token")[0].value;
-    fetch("/comments/preview", {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ content: content, authenticity_token: csrf_token })
-    }).then(response => response.json())
-    .then(data => {
-        document.getElementById("comment-preview").innerHTML = data.preview;
-        document.getElementById("comment-preview").style.display = "block";
+      let content = document.getElementById("comment_body").value;
+      let csrf_token = document.getElementsByName("authenticity_token")[0].value;
+      fetch("/comments/preview", {
+          method: "POST",
+          headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ content: content, authenticity_token: csrf_token })
+      }).then(response => response.json())
+      .then(data => {
+          document.getElementById("comment-preview").innerHTML = data.preview;
+          document.getElementById("comment-preview").style.display = "block";
+      });
     });
-  });
+  }
 
-  document.getElementById("comment-form-toggle").addEventListener("click", (e) => {
-    document.getElementById("comment-preview").style.display = "none";
-    document.getElementById("comment-form").style.display = "block";
-    document.getElementById("comment-form-toggle").classList.add("active");
-    document.getElementById("comment-preview-toggle").classList.remove("active");
-  });
+  let commentFormToggle = document.getElementById("comment-form-toggle");
+  if (commentFormToggle !== null) {
+    document.getElementById("comment-form-toggle").addEventListener("click", (e) => {
+      document.getElementById("comment-preview").style.display = "none";
+      document.getElementById("comment-form").style.display = "block";
+      document.getElementById("comment-form-toggle").classList.add("active");
+      document.getElementById("comment-preview-toggle").classList.remove("active");
+    });
+  }
 });
