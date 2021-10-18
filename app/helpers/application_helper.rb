80e17f5b9c4e64479e201f495b0fa03a5219bc11
Brian-Demon <BrianWMatejka@gmail.com>
2021-10-18 16:46:21 -0500

    `page_link_tree` helper method: - Use for link tree usually at the top of a category/discussion page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
       image_tag(user.image, width: width)
     end
   end
+
+  def page_link_tree(category, discussion)
+    root_link = link_to("Conclave", categories_path)
+    category_link = link_to(category.name, category)
+    discussion_link = link_to(discussion.title, [category, discussion]) if discussion
+    [root_link, category_link, discussion_link].compact.join(" > ").html_safe
+  end
 end
