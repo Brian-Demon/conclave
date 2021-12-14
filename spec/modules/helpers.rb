a6f7bc88d125239179658dbc8461d0a30d324c0b
Brian-Demon <BrianWMatejka@gmail.com>
2021-12-14 16:00:08 -0600

    Move expectations out of matchers

--- spec/modules/helpers.rb
+++ spec/modules/helpers.rb
@@ -89,8 +89,8 @@ module DiscussionHelpers
   include SharedMatchers
   include DiscussionMatchers
 
-  def create_discussion(discussion_title, discussion_body, category_name = DEFAULT_CATEGORY_NAME)
-    create_category(category_name, CATEGORY_DESCRIPTION)
+  def create_discussion(discussion_title, discussion_body)
+    visit category_path(Category.create(name: DEFAULT_CATEGORY_NAME, position: 1, description: CATEGORY_DESCRIPTION))
 
     find_button "Create Discussion"
     click_on "Create Discussion"
@@ -98,70 +98,21 @@ module DiscussionHelpers
     fill_in("Title", with: discussion_title)
     fill_in("Body", with: discussion_body)
     click_on "Submit"
-    expect(page).to have_link_tree(category_name, DISCUSSION_TITLE)
-    expect(page).to have_correct_discussion_title_on_page(discussion_title)
-    expect(page).to have_correct_discussion_message_for_action("created")
-    expect(page).to have_expected_discussion_buttons
-    discussion = Discussion.find_by(title: DISCUSSION_TITLE, body: DISCUSSION_BODY)
-    within "#comment-section" do
-      expect(page).to have_write_comment_section
-    end
-    within "#user-info" do
-      expect(page).to have_correct_user_info(discussion)
-    end
-
-    # Check to make sure all the info for the newly created Discussion are correct
-    within "#link-tree" do
-      click_link category_name
-    end
-    expect(page).to have_the_expected_category_header("DISCUSSION REPLIES LAST POST")
-    within "#discussion-#{discussion.id}" do
-      find_by_id("discussion-#{discussion.id}-link")
-    end
-    within "#discussion-#{discussion.id}-info" do
-      expect(page).to have_text(discussion.user.login)
-      expect(page).to have_text(discussion.created_at.strftime("%m/%d/%Y %I:%M%p"))
-    end
-    within "#discussion-#{discussion.id}-replies .reply-counter" do
-      expect(page).to have_text(discussion.comments.count)
-    end
-
-    # Return back to the discussion#show for the newly created Discussion so the rest of the specs can work correctly
-    within "#discussion-#{discussion.id}-link" do
-      click_link DISCUSSION_TITLE
-    end
   end
 
-  def edit_discussion(updated_title = "New Title", updated_body = "New Body", category_name = DEFAULT_CATEGORY_NAME, discussion_id)
-    create_discussion(DISCUSSION_TITLE, DISCUSSION_BODY)
-    discussion = Discussion.find_by(title: DISCUSSION_TITLE, body: DISCUSSION_BODY)
-
+  def edit_discussion(updated_title = "New Title", updated_body = "New Body")
     within "#discussion-title-buttons" do
       click_on "Edit"
     end
     fill_in("Title", with: updated_title)
     fill_in("Body", with: updated_body)
     click_on "Submit"
-    expect(page).to have_link_tree(category_name, updated_title)
-    expect(page).to have_correct_discussion_title_on_page(updated_title)
-    expect(page).to have_correct_discussion_message_for_action("updated")
-    expect(page).to have_expected_discussion_buttons
-    within "#user-info" do
-      expect(page).to have_correct_user_info(discussion)
-    end
-    within "#comment-section" do
-      expect(page).to have_write_comment_section
-    end
   end
 
-  def delete_category(discussion_title, discussion_body)
-    create_discussion(DISCUSSION_TITLE, DISCUSSION_BODY)
-    discussion_id = Discussion.find_by(title: discussion_title, body: discussion_body).id
+  def delete_discussion
     within "#discussion-title-buttons" do
       click_on "Delete"
     end
     page.driver.browser.switch_to.alert.accept
-    expect(page).to have_text("Discussion was successfully deleted.")
-    expect(page).to_not have_selector("#discussion_#{discussion_id}")
   end
 end
--- spec/system/discussion_spec.rb
+++ spec/system/discussion_spec.rb
@@ -1,20 +1,83 @@
 require "rails_helper"
 require_relative "../modules/helpers"
+require_relative "../matchers/category_matchers"
+require_relative "../matchers/discussion_matchers"
+require_relative "../matchers/shared_matchers"
 
-RSpec.describe "Discussion", type: :system do
+RSpec.describe "Discussion,", type: :system do
+  include SharedMatchers
   include SessionHelpers
   include CategoryHelpers
   include DiscussionHelpers
 
-  it "can be created" do
-    create_discussion("Test Discussion Title", "Test discussion body")
-  end
+  context "when logged in as admin," do
+    before(:each) do
+      login_as("admin")
+    end
 
-  it "can be edited" do
-    edit_discussion("Test Discussion Title", "Test discussion body")
-  end
+    let(:category_name) { "Test Category Name" }
+    let(:discussion_title) { "Test Discussion Title" }
+    let(:discussion_body) { "Test discussion body" }
 
-  it "can be deleted" do
-    delete_category("Test Discussion Title", "Test discussion body")
+    it "can be created" do
+      create_discussion(discussion_title, discussion_body)
+      discussion = Discussion.find_by(title: discussion_title, body: discussion_body)
+
+      expect(page).to have_link_tree(category_name, discussion_title)
+      expect(page).to have_correct_discussion_title_on_page(discussion_title)
+      expect(page).to have_correct_discussion_message_for_action("created")
+      expect(page).to have_expected_discussion_buttons
+      within "#comment-section" do
+        expect(page).to have_write_comment_section
+      end
+      within "#user-info" do
+        expect(page).to have_correct_user_info(discussion)
+      end
+      within "#link-tree" do
+        click_link category_name
+      end
+      expect(page).to have_the_expected_category_header("DISCUSSION REPLIES LAST POST")
+      within "#discussion-#{discussion.id}" do
+        find_by_id("discussion-#{discussion.id}-link")
+      end
+      within "#discussion-#{discussion.id}-info" do
+        expect(page).to have_text(discussion.user.login)
+        expect(page).to have_text(discussion.created_at.strftime("%m/%d/%Y %I:%M%p"))
+      end
+      within "#discussion-#{discussion.id}-replies .reply-counter" do
+        expect(page).to have_text(discussion.comments.count)
+      end
+    end
+
+    it "can be edited" do
+      updated_title = "New Title"
+      updated_body = "New Body"
+
+      create_discussion(discussion_title, discussion_body)
+      discussion = Discussion.find_by(title: discussion_title, body: discussion_body)
+
+      edit_discussion(updated_title, updated_body)
+
+      expect(page).to have_link_tree(category_name, updated_title)
+      expect(page).to have_correct_discussion_title_on_page(updated_title)
+      expect(page).to have_correct_discussion_message_for_action("updated")
+      expect(page).to have_expected_discussion_buttons
+      within "#user-info" do
+        expect(page).to have_correct_user_info(discussion)
+      end
+      within "#comment-section" do
+        expect(page).to have_write_comment_section
+      end
+    end
+
+    it "can be deleted" do
+      create_discussion(discussion_title, discussion_body)
+      discussion = Discussion.find_by(title: discussion_title, body: discussion_body)
+
+      delete_discussion
+
+      expect(page).to have_text("Discussion was successfully deleted.")
+      expect(page).to_not have_selector("#discussion_#{discussion.id}")
+    end
   end
 end
